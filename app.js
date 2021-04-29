553c6b0757b8fb1e986bcd8786169da98430ed0a
Bardia Biramzadeh <bardia-bb@gmx.de>
2021-04-29 08:58:46 +0200

    push before merge

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const projectName = "Green-Tower";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 
 // SET UP SESSION
 const session = require("express-session");
